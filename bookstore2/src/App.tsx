6b2b7ea8d60df35bf19679f7e21852a67609c34e
tranphuong670 <tranhongphuong272@gmail.com>
2025-09-08 15:30:32 +0700

    fix all file

---
+++ bookstore2/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Box } from '@mui/material'
+import CrudDashboard from './crud-dashboard/CrudDashboard'
+
+function App() {
+
+  return (
+    <Box>
+      <CrudDashboard/>
+    </Box>
+  )
+}
+
+export default App
